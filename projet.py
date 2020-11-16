e8e45a135e70292bb8350aac45fee60267794f20
Natuy <35492739+Natuy@users.noreply.github.com>
2020-11-16 12:17:47 +0100

    pounch

--- projet.py
+++ projet.py
@@ -1,59 +1,62 @@
 from Crypto.Cipher import AES
-import io
-import os
 import cv2
+import random
 from PIL import Image
 
 key= b'1234567890123456'
+dh_key= b'1234567890123456'
 iv= b'1234567890123456'
-file_name ="./TurtleD.tif"
+file_name ="./14_Perroquet.ppm"
+
+message = 'je suis super secret'
 
 
 #Encrypting Image
-def encrypt_image():
-    global key,iv,file_name
+def encrypt_image(filename_in,filename_out):
+    global key,iv
     
-    input_file = open(file_name,"rb")
+    input_file = open(filename_in,"rb")
     input_data = input_file.read()
     input_file.close()
     
-    cfb_cipher = AES.new(key, AES.MODE_CFB, iv)
+    cfb_cipher = AES.new(key, AES.MODE_CFB,iv)
     enc_data = cfb_cipher.encrypt(input_data)
     
-    enc_file = open(file_name+".enc", "wb")
+    enc_file = open(filename_out, "wb")
     enc_file.write(enc_data)
     enc_file.close()
 	
-    img = cv2.imread(file_name)
-    cv2.namedWindow('img',cv2.WINDOW_NORMAL) # Can be resized
-    cv2.imshow('img',img)
-    cv2.waitKey(0)
+    #img = cv2.imread(file_name)
+    #cv2.namedWindow('img',cv2.WINDOW_NORMAL) # Can be resized
+    #cv2.imshow('img',img)
+    #cv2.waitKey(0)
     
     
-def decrypt_image():
+def decrypt_image(filename_in,filename_out):
     global key,iv,entry_for_folder
    
-    enc_file2 = open((file_name)+'.enc',"rb")
+    enc_file2 = open(filename_in,"rb")
     enc_data2 = enc_file2.read()
     enc_file2.close()
 
-    cfb_decipher = AES.new(key, AES.MODE_CFB, iv)
+    cfb_decipher = AES.new(key, AES.MODE_CFB,iv)
     plain_data = (cfb_decipher.decrypt(enc_data2))
 
-    dec_file = open((file_name[:-8])+'dec.jpg', "wb")
+    dec_file = open(filename_out, "wb")
     dec_file.write(plain_data)
     dec_file.close()
 
-def save_enc_file(filename):
+def save_file(filename):
     
-    input_file = open(filename+".enc", 'rb')
+    input_file = open(filename, 'rb')
     input_data = input_file.read()
     input_file.close()
      
     
     #then treat encrypted data as a bitmap
     # calculate sizes
-    imgBase = cv2.imread(filename)              
+    imgBase = cv2.imread(filename)   
+           
     H = imgBase.shape[0]          # W=H, such that everything fits in
     W = imgBase.shape[1]
     
@@ -61,8 +64,52 @@ def save_enc_file(filename):
     #imagedata = imgC + '\0' * (W*H*3 - len(imgC))
     
     image = Image.frombytes('RGB', (W, H), input_data)         # create image
-    image.save('imageC.bmp')  
-                
-encrypt_image();
-decrypt_image();
-save_enc_file(file_name)
+    image.save(filename+'.bmp')  
+              
+def hide_message(filename_in,filename_out):
+    input_file = open(filename_in, 'rb')
+    input_data = input_file.read()
+    input_file.close()
+
+    generator = random.Random()
+    generator.seed(dh_key)
+    data_size = len(input_data)
+    #mess=bytearray(message,'utf8')
+    data=bytearray(input_data)
+    for i in range(0,data_size//128):
+        data[128*i + generator.randrange(128)]=i%2
+    
+    hid_file = open(filename_out, "wb")
+    hid_file.write(data)
+    hid_file.close()
+    
+def find_message(filename_in):
+    input_file = open(filename_in, 'rb')
+    input_data = input_file.read()
+    input_file.close()
+
+    mess=bytes(message,'utf8')
+    
+    generator = random.Random()
+    generator.seed(dh_key)
+    data_size = len(input_data)
+    
+    data=bytearray(input_data)
+    for i in range(0,data_size//128):
+        print(data[128*i + generator.randrange(128)])
+    
+    
+#save_file(file_name)
+encrypt_image(file_name,file_name[:2]+'enc'+file_name[2:])
+#save_file(file_name[:2]+'enc'+file_name[2:])
+print("encryption complete")
+hide_message(file_name[:2]+'enc'+file_name[2:],file_name[:2]+'hid'+file_name[2:])
+#save_file(file_name[:2]+'hid'+file_name[2:])
+print("DH complete")
+
+find_message(file_name[:2]+'hid'+file_name[2:])
+print("Data recuperation complete")
+
+decrypt_image(file_name[:2]+'hid'+file_name[2:],file_name[:2]+'dec'+file_name[2:])
+#save_file(file_name[:2]+'dec'+file_name[2:])
+print("decryption complete")
